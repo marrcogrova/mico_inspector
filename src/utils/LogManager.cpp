0192ebaff1ad52a5057a2a0f4069211096ddc7da
pabramsor <pabramsor@gmail.com>
2018-08-04 11:13:36 +0200

    Added color to logs and added interface class for classes

--- src/utils/LogManager.cpp
+++ src/utils/LogManager.cpp
@@ -22,76 +22,89 @@
 
 #include <rgbd_tools/utils/LogManager.h>
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
-LogManager *LogManager::mSingleton = nullptr;
+namespace rgbd{
 
-//---------------------------------------------------------------------------------------------------------------------
-void LogManager::init(const string _appName) {
-	if (!mSingleton)
-        mSingleton = new LogManager(_appName);
-	else
-		mSingleton->warning("Someone tried to reinitialize the Log Manager");
-}
+	LogManager *LogManager::mSingleton = nullptr;
 
-//---------------------------------------------------------------------------------------------------------------------
-void LogManager::close(){
-	delete mSingleton;
-}
 
-//---------------------------------------------------------------------------------------------------------------------
-LogManager * LogManager::get(){
-	return mSingleton;
-}
+	const LogManager::ColorHandle LogManager::cTextRed		= "\033[31m";
+	const LogManager::ColorHandle LogManager::cTextYellow	= "\033[33m";
+	const LogManager::ColorHandle LogManager::cTextBlue		= "\033[34m";
+	const LogManager::ColorHandle LogManager::cTextGreen	= "\033[32m";
+	const LogManager::ColorHandle LogManager::cTextReset	= "\033[0m";
 
-//---------------------------------------------------------------------------------------------------------------------
-void LogManager::message(const std::string & _tag, const std::string & _msg, bool _useCout) {
-	double timeSpan = std::chrono::duration<double>(chrono::high_resolution_clock::now() - mInitTime).count();
-	std::string logLine = to_string(timeSpan) + "\t [" + _tag + "] " + _msg + "\n";
-	mSecureGuard.lock();
-	mLogFile << logLine;
-	mLogFile.flush();
-	mSecureGuard.unlock();
-    if(_useCout){
-		cout << logLine;
-        cout.flush();
+	//---------------------------------------------------------------------------------------------------------------------
+	void LogManager::init(const string _appName) {
+		if (!mSingleton)
+			mSingleton = new LogManager(_appName);
+		else
+			mSingleton->warning("Someone tried to reinitialize the Log Manager");
 	}
-}
 
-//---------------------------------------------------------------------------------------------------------------------
-void LogManager::status(const std::string & _msg, bool _useCout){
-    message("STATUS", _msg, _useCout);
-}
+	//---------------------------------------------------------------------------------------------------------------------
+	void LogManager::close(){
+		delete mSingleton;
+	}
 
-//---------------------------------------------------------------------------------------------------------------------
-void LogManager::warning(const std::string & _msg, bool _useCout){
-    message("WARNING",_msg, _useCout);
-}
+	//---------------------------------------------------------------------------------------------------------------------
+	LogManager * LogManager::get(){
+		assert(mSingleton != nullptr);
+		return mSingleton;
+	}
 
-//---------------------------------------------------------------------------------------------------------------------
-void LogManager::error(const std::string & _msg, bool _useCout){
-    message("ERROR", _msg,_useCout);
-}
+	//---------------------------------------------------------------------------------------------------------------------
+	void LogManager::message(const std::string & _tag, const std::string & _msg, bool _useCout, ColorHandle _color) {
+		double timeSpan = std::chrono::duration<double>(chrono::high_resolution_clock::now() - mInitTime).count();
+		std::string logLine = to_string(timeSpan) + "\t [" + _tag + "] " + _msg;
+		mSecureGuard.lock();
+		mLogFile << logLine;
+		mLogFile.flush();
+		mSecureGuard.unlock();
+		logLine = _color + logLine + cTextReset + "\n";
+		if(_useCout){
+			cout << logLine;
+			cout.flush();
+		}
+	}
 
-//---------------------------------------------------------------------------------------------------------------------
-void LogManager::saveTimeMark(string _tag) {
-    mTimeMap[_tag] = std::chrono::high_resolution_clock::now();
-}
+	//---------------------------------------------------------------------------------------------------------------------
+	void LogManager::status(const std::string & _msg, bool _useCout){
+		message("STATUS", _msg, _useCout, cTextBlue);
+	}
 
-//---------------------------------------------------------------------------------------------------------------------
-double LogManager::measureTimeBetweenMarks(string _tag1, string _tag2) {
-    return std::chrono::duration<double>(mTimeMap[_tag1] - mTimeMap[_tag2]).count();
-}
+	//---------------------------------------------------------------------------------------------------------------------
+	void LogManager::warning(const std::string & _msg, bool _useCout){
+		message("WARNING",_msg, _useCout, cTextYellow);
+	}
 
-//---------------------------------------------------------------------------------------------------------------------
-LogManager::LogManager(const std::string _appName) {
-	mLogFile.open(_appName + to_string(time(NULL))+".txt");
-	mInitTime = chrono::high_resolution_clock::now();
-	status("Initialized log");
-}
+	//---------------------------------------------------------------------------------------------------------------------
+	void LogManager::error(const std::string & _msg, bool _useCout){
+		message("ERROR", _msg,_useCout, cTextRed);
+	}
 
-//---------------------------------------------------------------------------------------------------------------------
-LogManager::~LogManager() {
-	mLogFile.close();
+	//---------------------------------------------------------------------------------------------------------------------
+	void LogManager::saveTimeMark(string _tag) {
+		mTimeMap[_tag] = std::chrono::high_resolution_clock::now();
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------
+	double LogManager::measureTimeBetweenMarks(string _tag1, string _tag2) {
+		return std::chrono::duration<double>(mTimeMap[_tag1] - mTimeMap[_tag2]).count();
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------
+	LogManager::LogManager(const std::string _appName) {
+		mLogFile.open(_appName + to_string(time(NULL))+".txt");
+		mInitTime = chrono::high_resolution_clock::now();
+		status("Initialized log");
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------
+	LogManager::~LogManager() {
+		mLogFile.close();
+	}
 }
