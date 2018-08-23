709f7a901e3b50df3f76890caba0af6f67f53b2e
pabramsor <pabramsor@gmail.com>
2018-08-23 12:11:11 +0200

    Moved EKF to separate folder, fixed compilation issues and added example of EKF

---
+++ examples/state_filtering/ekf/main.cpp
@@ -0,0 +1,103 @@
+//---------------------------------------------------------------------------------------------------------------------
+//  RGBD_TOOLS
+//---------------------------------------------------------------------------------------------------------------------
+//  Copyright 2018 Pablo Ramon Soria (a.k.a. Bardo91) pabramsor@gmail.com
+//---------------------------------------------------------------------------------------------------------------------
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//  and associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------
+
+#include <rgbd_tools/state_filtering/ExtendedKalmanFilter.h>
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+class Simple2DEKF: public rgbd::ExtendedKalmanFilter<float,4,2> {
+protected:
+    //---------------------------------------------------------------------------------------------------
+    void updateJf(const double _incT){
+        mJf.setIdentity();
+    }
+
+    //---------------------------------------------------------------------------------------------------
+    void updateHZk(){
+        mHZk = mXak.block<2,1>(0,0);
+    }
+
+    //---------------------------------------------------------------------------------------------------
+    void updateJh(){
+        mJh.setIdentity();
+    }
+};
+
+int main(int _argc, char **_argv){
+
+    const float NOISE_LEVEL = 0.1;
+
+    Eigen::Matrix<float, 4, 4> mQ; // State covariance
+    mQ.setIdentity();   
+    mQ.block<2,2>(0,0) *= 0.001;
+    mQ.block<2,2>(2,2) *= 0.001;
+
+    Eigen::Matrix<float, 2, 2> mR; // Observation covariance
+    mR.setIdentity();   
+    mR.block<2,2>(0,0) *= NOISE_LEVEL;
+
+    Eigen::Matrix<float, 4,1> x0;
+    x0 <<   1,0,    // (x,y)
+            0,0;    // (v_x, v_y)
+    
+    Simple2DEKF ekf;
+
+    ekf.setUpEKF(mQ, mR, x0);
+    cv::Mat map = cv::Mat::zeros(cv::Size(300, 300), CV_8UC3);
+
+    cv::namedWindow("display", CV_WINDOW_FREERATIO);
+    cv::Point2f prevObs(250, 150), prevState(250, 150);
+
+    float fakeTimer = 0;
+
+    while(true){
+        Eigen::Matrix<float, 2,1> z;    // New observation
+        z <<    0.5 + cos(fakeTimer*1)*0.5 + ((double)rand())/RAND_MAX*NOISE_LEVEL,
+                0.5 + sin(fakeTimer*1)*0.5 + ((double)rand())/RAND_MAX*NOISE_LEVEL;
+
+        fakeTimer += 0.03;
+
+        ekf.stepEKF(z, 0.03);
+
+        Eigen::Matrix<float,4,1> filteredX = ekf.state();
+
+        cv::Point2f currentObs(
+            z[0]*200 + 50,
+            z[1]*200 + 50
+        );
+        cv::Point2f currentState(
+            filteredX[0]*200 + 50,
+            filteredX[1]*200 + 50
+        );
+
+        cv::line(map, prevObs, currentObs, cv::Scalar(100,100,255));
+        cv::line(map, prevState, currentState, cv::Scalar(0,255,0),2);
+
+        prevObs = currentObs;
+        prevState = currentState;
+        
+        cv::imshow("display", map);
+        cv::waitKey(30);
+
+    }
+
+}
