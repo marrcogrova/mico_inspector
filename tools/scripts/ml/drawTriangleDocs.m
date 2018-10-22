03c3be9d9e778a07f356ff1734aef6841fc28df5
pabramsor <pabramsor@gmail.com>
2018-10-22 20:58:17 +0200

    added some scripts for visualizing lda results

---
+++ tools/scripts/ml/drawTriangleDocs.m
@@ -0,0 +1,23 @@
+function [] = drawTriangleDocs(theta, gt)
+
+
+vertexs = [ 1 0 0;
+          0 1 0;
+          0 0 1];
+       
+%figure();
+
+[M K] = size(theta);
+
+points = [];
+for m = 1:M
+   docProb = theta(m,:);
+   point = docProb*vertexs;
+   points = [points ; point];
+end
+
+scatter3(points(:,1),points(:,2),points(:,3));
+hold on;
+scatter3(vertexs(:,1),vertexs(:,2),vertexs(:,3), 'r*');
+
+end
