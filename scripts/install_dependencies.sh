65ff34d61c568a6bf631b911cc0ec4f0dfbd65bb
Bardo91 <19Bardo91@gmail.com>
2019-09-24 22:21:19 +0200

    Added more dependencies to install script and added fixes related with g2o and vtk

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -55,7 +55,7 @@ fi
 read -r -p "Do you want to install latest version of PCL [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-	sudo apt-get install libeigen3-dev libflann-dev libvtk7-dev libboost1.65-dev libqhull-dev libomp-dev libsuitesparse-dev
+	sudo apt-get install libeigen3-dev libflann-dev libvtk6-dev libboost1.65-dev libqhull-dev
     install_git_repo "pcl" "https://github.com/PointCloudLibrary/pcl"
 fi
 
@@ -63,6 +63,25 @@ fi
 ###################################################################
 ###########				INSTALL SLAM DEPS				###########
 ###################################################################
+sudo apt-get install liblapack-dev libblas-dev
+sudo apt-get install libomp-dev libsuitesparse-dev libcholmod3
+
+install_git_repo "eigen-git-mirror" "https://github.com/eigenteam/eigen-git-mirror" "3.3.7"
+
 install_git_repo "DBoW2" "https://github.com/dorian3d/DBoW2"
+
 install_git_repo "DLoopDetector" "https://github.com/Bardo91/DLoopDetector"
+
 install_git_repo "g2o" "https://github.com/RainerKuemmerle/g2o"
+
+# HACKING g2oconfig.cmake because it complains about not finding eigen for some reason
+sudo rm /usr/local/lib/cmake/g2o/g2oConfig.cmake -f
+
+echo "include(CMakeFindDependencyMacro)
+
+find_dependency(OpenGL)
+
+include("/usr/local/lib/cmake/g2o/g2oTargets.cmake")" | sudo tee -a /usr/local/lib/cmake/g2o/g2oConfig.cmake
+
+# It may happens that vtk lib link without version is not created but still referenced, so create a soft link 
+sudo ln -s /usr/bin/vtk6 /usr/bin/vtk
