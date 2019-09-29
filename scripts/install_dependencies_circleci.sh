d3c3930f891c95048e04a535064a50b1f669b277
Bardo91 <19Bardo91@gmail.com>
2019-09-29 10:28:46 +0200

    Added installation of qt5 and improved travis script

--- scripts/install_dependencies_circleci.sh
+++ scripts/install_dependencies_circleci.sh
@@ -32,15 +32,15 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get install -y ros-kinetic-desktop-full
 
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt install ros-kinetic-opencv3
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install -y ros-kinetic-opencv3
 
 
 ###################################################################
@@ -57,5 +57,7 @@ install_git_repo "DLoopDetector" "https://github.com/Bardo91/DLoopDetector"
 
 install_git_repo "g2o" "https://github.com/RainerKuemmerle/g2o"
 
+
+sudo apt-get install -y qt5-default
 install_git_repo "nodeeditor" "https://github.com/paceholder/nodeeditor"
  
