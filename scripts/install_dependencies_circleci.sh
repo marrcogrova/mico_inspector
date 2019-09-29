d70ded00fc2df15d71007e29bd0ef91f50c73196
Bardo91 <19Bardo91@gmail.com>
2019-09-29 22:37:39 +0200

    testing install opencv from apt

--- scripts/install_dependencies_circleci.sh
+++ scripts/install_dependencies_circleci.sh
@@ -28,23 +28,23 @@ install_git_repo () {
 ###########		INSTALL OPENCV and OPENCV contrib		###########
 ###################################################################
 
-if [ -d "opencv" ] 
-then
-	echo "Library $1 already installed" 
-else
-	git clone "https://github.com/opencv/opencv_contrib"
-	git clone "https://github.com/opencv/opencv"
-	cd opencv
-	mkdir build; cd build
-	cmake .. -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -DWITH_TBB=OFF -DWITH_OPENMP=OFF -DWITH_IPP=OFF -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_EXAMPLES=OFF -DWITH_NVCUVID=OFF -DWITH_CUDA=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_CSTRIPES=OFF -DWITH_OPENCL=OFF -DBUILD_JAVA=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_js=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_dnn=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_objdetect=OFF
-	make -j$(nproc)
-	sudo make install 
-	cd ../..
-fi
+# if [ -d "opencv" ] 
+# then
+# 	echo "Library $1 already installed" 
+# else
+# 	git clone "https://github.com/opencv/opencv_contrib"
+# 	git clone "https://github.com/opencv/opencv"
+# 	cd opencv
+# 	mkdir build; cd build
+# 	cmake .. -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -DWITH_TBB=OFF -DWITH_OPENMP=OFF -DWITH_IPP=OFF -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_EXAMPLES=OFF -DWITH_NVCUVID=OFF -DWITH_CUDA=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_CSTRIPES=OFF -DWITH_OPENCL=OFF -DBUILD_JAVA=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_js=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_dnn=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_objdetect=OFF
+# 	make -j$(nproc)
+# 	sudo make install 
+# 	cd ../..
+# fi
 
-sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+# sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 
-sudo ldconfig
+# sudo ldconfig
 
 ###################################################################
 ###########				INSTALL SLAM DEPS				###########
