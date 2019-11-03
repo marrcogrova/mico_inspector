d6b84e93f7752c17c72e3bae2da02658050846ac
Bardo91 <19Bardo91@gmail.com>
2019-11-03 16:34:35 +0100

    restored multithreading

--- modules/flow/src/Policy.cpp
+++ modules/flow/src/Policy.cpp
@@ -93,8 +93,8 @@ namespace mico{
                 for(auto&tag:maskTags){ // uff... For more complex pipelines with shared data might not work... need conditions per callback.
                     validData_[tag] = false;
                 }
-                std::thread(pairCb.second, dataFlow_).detach(); // 666 Smthg is not completelly thread safe and produces crash
-                //pairCb.second(dataFlow_);
+                // std::thread(pairCb.second, dataFlow_).detach(); // 666 Smthg is not completelly thread safe and produces crash
+                pairCb.second(dataFlow_);
             }
         }
     }
