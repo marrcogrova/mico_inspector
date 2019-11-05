62e221ca74878b434f5cb8219ad61f5f8e3a4e6d
ric92 <ricloplop@gmail.com>
2019-11-05 17:00:10 +0100

    Added entity class to dnn module

---
+++ modules/dnn/include/mico/dnn/map3d/Entity.h
@@ -0,0 +1,76 @@
+//---------------------------------------------------------------------------------------------------------------------
+//  mico
+//---------------------------------------------------------------------------------------------------------------------
+//  Copyright 2018 Pablo Ramon Soria (a.k.a. Bardo91) pabramsor@gmail.com & Ricardo Lopez Lopez (a.k.a Ric92)
+//---------------------------------------------------------------------------------------------------------------------
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//  and associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------
+
+#ifndef MICO_DNN_MAP3D_ENTITY_H_
+#define MICO_DNN_MAP3D_ENTITY_H_
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <opencv2/opencv.hpp>
+
+namespace mico
+{
+
+template <typename PointType_>
+struct Entity
+{
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+public:
+    typedef std::shared_ptr<Entity<PointType_>> Ptr;
+
+    Entity();
+
+
+    void updatePose(Eigen::Matrix4f &_pose);
+
+    Eigen::Matrix4f getPose();
+
+    typename pcl::PointCloud<PointType_>::Ptr getCloud();
+
+private:
+
+    int id;
+
+    /// spatial data 
+    Eigen::Matrix4f pose = Eigen::Matrix4f::Identity();
+    Eigen::Vector3f position;
+    Eigen::Quaternionf orientation;
+    
+    /// 3D
+    typename pcl::PointCloud<PointType_>::Ptr cloud;
+
+    /// 2D
+    std::vector<cv::Point2f> projections;
+    cv::Mat descriptors;
+
+    /// detection 
+    int label;    
+    float confidence;
+    std::vector<float> boundingbox;     // left top right bottom
+    
+    /// visibility
+    std::map<int,Eigen::Matrix4f> covisibility;
+
+};
+} // namespace mico
+#include <mico/dnn/map3d/Entity.inl>
+
+#endif
